23df28aec41b9f4cfbc3182fb30c12cc4025065c
Rusheel Dasari <rusheel@gmail.com>
2022-01-06 17:54:57 -0800

    GameFunctions refactor

--- GameFunctions.py
+++ GameFunctions.py
@@ -1,28 +1,29 @@
+from Color import Color
 from Board import Board
+
 class GameFunctions():
-    turn = 0
 
     def __init__(self, player1, player2):
         self.player1 = player1
         self.player2 = player2
-    
-        
+        self.turn = -1
+        self.board = Board()
+        self.switch_turns()
 
     def switch_turns(self):
-        self.turn = self.turn % 2
+        self.turn = self.turn * -1  
+        return self.get_turn_message()
 
-        if self.turn == 0:
+    def get_turn_message(self):
+        if self.turn == 1:
             print(f"It's {self.player1} turn!")
-            self.turn += 1
+            return(f"It's {self.player1} turn!")
         else:
             print(f"It's {self.player2} turn!")
-            self.turn += 1
-    
-    def start_game(self):
-        self.board = Board()
-
-    def play(self, tile, column):
-        self.board.dropTile(column, tile.color)
+            return(f"It's {self.player2} turn!")
         
+    def playTile(self, column):
+        self.board.dropTile(column, Color(self.turn))
+        self.switch_turns()      
 
     
