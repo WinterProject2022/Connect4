6927d67a39e7d90ff4178323ab240cbc1516574e
Nickolas Karapanos <karapanos@Nickolass-MacBook-Pro.local>
2022-01-07 19:29:54 -0600

    added documentation to classes

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ a = 5
 
 print(a)
 
+from Tile import Tile 
+from Color import Color
 from GameFunctions import GameFunctions
 
-newGame = GameFunctions("Saanvi", "Micheal")
+#newGame = GameFunctions("Saanvi", "Micheal")
 
-newGame.switch_turns()
-newGame.switch_turns()
+#newGame.switch_turns()
+#newGame.switch_turns()
 
-
+tile = Tile(Color.RED,2,3)
+print(tile)
 """ 
 def main():
     
